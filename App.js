7149cd4c7bcc4611cda9e373f02f62bb7cf7bf06
GautamKumar007 <jangragautam98@gmail.com>
2022-02-19 17:03:52 +0530

    structure

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my first App</Text>
+      <View style={styles.show}></View>
+      <View style={styles.calculations}></View>
+      <View style={styles.Buttons}>
+        <View style={styles.numbers}>
+          <View style={styles.row}>
+            <Button title='1'></Button>
+            <Button title='2'></Button>
+            <Button title='3'></Button>
+          </View>
+          <View style={styles.row}>
+            <Button title='4'></Button>
+            <Button title='5'></Button>
+            <Button title='6'></Button>
+          </View>
+          <View style={styles.row}>
+            <Button title='7'></Button>
+            <Button title='8'></Button>
+            <Button title='9'></Button>
+          </View>
+          <View style={styles.row}>
+            <Button title='0'></Button>
+            <Button title='.'></Button>
+            <Button title='#'></Button>
+          </View>
+        </View>
+        <View style={styles.oprators}>
+            <Button title='+'></Button>
+            <Button title='-'></Button>
+            <Button title='*'></Button>
+            <Button title='%'></Button>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  row: {
+    flex: 1,
+    flexGrow: 1,
+    flexDirection: 'row',
+  },
+  show: {
+    flex: 2,
+    backgroundColor: 'white',
+  },
+  calculations: {
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  Buttons: {
+    flex: 7,
+    flexDirection: 'row',
+  },
+  numbers: {
+    flex: 2,
+    backgroundColor: 'grey',
+  },
+  oprators: {
+    flex: 1,
+    backgroundColor: 'green',
+  }
+
 });
